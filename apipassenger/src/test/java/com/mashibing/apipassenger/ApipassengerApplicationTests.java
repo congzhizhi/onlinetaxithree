6f794ec70ed24091456b0ec5c49947e3c462653e
congzhizhi <congzhizhi@163.com>
2021-02-09 18:28:18 +0800

    网约车项目

---
+++ apipassenger/src/test/java/com/mashibing/apipassenger/ApipassengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing.apipassenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipassengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
