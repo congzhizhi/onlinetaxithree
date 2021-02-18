3ad7cdd840f5e3628b135871440a42637daa2328
congzhizhi <congzhizhi@163.com>
2021-02-18 16:44:58 +0800

    20210212

--- cloud-eureka/src/main/java/com/mashibing/cloudeureka/listen/TestEvent.java
+++ cloud-eureka/src/main/java/com/mashibing/cloudeureka/listen/TestEvent.java
@@ -1,6 +1,8 @@
 package com.mashibing.cloudeureka.listen;
 
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegisteredEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRenewedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +16,13 @@ public class TestEvent {
     @EventListener
     public void event(EurekaInstanceCanceledEvent canceledEvent){
         System.out.println("服务下线："+canceledEvent.getAppName()+":"+canceledEvent.getServerId()+":"+canceledEvent.getSource());
+    }
+    @EventListener
+    public void eventRegistry(EurekaInstanceRegisteredEvent registeredEvent){
+        System.out.println("服务上线："+registeredEvent.getInstanceInfo().toString());
+    }
+    @EventListener
+    public void renewRegistry(EurekaInstanceRenewedEvent registeredEvent){
+        System.out.println("服务续约："+registeredEvent.getInstanceInfo().toString());
     }
 }
